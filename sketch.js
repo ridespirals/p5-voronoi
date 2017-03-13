72a4fe85a74a0930bed83d955531b24ded97d538
John Varga <john.g.varga@gmail.com>
2017-03-12 22:36:15 -0700

    create voronoi diagram

--- sketch.js
+++ sketch.js
@@ -1,19 +1,48 @@
 // for red, green, and blue color values
-var r, g, b;
+const sites = []
+const voronoi = new Voronoi()
+let diagram = null
+
+const TotalSites = 40
 
 function setup() {
-  createCanvas(720, 400);
-  // Pick colors randomly
-  r = random(255);
-  g = random(255);
-  b = random(255);
+    const w = 720
+    const h = 480
+    createCanvas(w, h)
+
+    const bbox = {
+        xl: 0, // x-left
+        xr: w, // x-right
+        yt: 0, // y-top
+        yb: h  // y-bottom
+    }
+
+    for (var i = 0; i < TotalSites; i++) {
+        sites.push(new p5.Vector(random(width), random(height)))
+    }
+
+    diagram = voronoi.compute(sites, bbox)
+
+    // console.group('Voronoi Diagram')
+    // console.debug('>> diagram', diagram)
+    // console.table(diagram)
+    // console.groupEnd('Voronoi Diagram')
 }
 
 function draw() {
-  background(127);
-  // Draw a circle
-  strokeWeight(2);
-  stroke(r, g, b);
-  fill(r, g, b, 127);
-  ellipse(360, 200, 200, 200);
+    background(51)
+
+    stroke(255)
+    strokeWeight(4)
+    noFill()
+
+    sites.forEach(p => {
+        point(p.x, p.y)
+    })
+
+    stroke(40, 210, 199)
+    strokeWeight(2)
+    diagram.edges.forEach(e => {
+        line(e.va.x, e.va.y, e.vb.x, e.vb.y)
+    })
 }
