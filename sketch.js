af0185963d6ca5c83770d4a06a3aeb0214744598
John Varga <john.g.varga@gmail.com>
2017-03-12 23:01:36 -0700

    colors

--- sketch.js
+++ sketch.js
@@ -1,9 +1,26 @@
-// for red, green, and blue color values
+// more colors:
+// http://www.colourlovers.com/palettes/search?query=neon
+// neon - http://www.colourlovers.com/palette/7315/Pop_Is_Everything
+//
+const colors = {
+    green: { r: 130, g: 230, b: 194 },
+    pink: { r: 242, g: 188, b: 224 },
+    purple: { r: 104, g: 143, b: 244 },
+    blue: { r: 5, g: 200, b: 246 },
+    cyan: { r: 3, g: 233, b: 248 }
+}
+const neon = {
+    green: [170,255,0],
+    orange: [255,170,0],
+    pink: [255,0,170],
+    purple: [170,0,255],
+    blue: [0,170,255]
+}
 const sites = []
 const voronoi = new Voronoi()
 let diagram = null
 
-const TotalSites = 40
+const TotalSites = 255
 
 function setup() {
     const w = 720
@@ -30,9 +47,9 @@ function setup() {
 }
 
 function draw() {
-    background(51)
+    background(40)
 
-    stroke(255)
+    stroke(neon.orange)
     strokeWeight(4)
     noFill()
 
@@ -40,9 +57,12 @@ function draw() {
         point(p.x, p.y)
     })
 
-    stroke(40, 210, 199)
+    // stroke(40, 210, 199)
+    stroke(neon.blue)
     strokeWeight(2)
     diagram.edges.forEach(e => {
         line(e.va.x, e.va.y, e.vb.x, e.vb.y)
     })
+
+    noLoop()
 }
